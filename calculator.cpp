0ab13702f2253d764d84389f6be8f76e57db36f2
anshumansinqh <anshuman2220singh@gmail.com>
2024-10-15 14:04:05 +0530

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+int sum, sub,prod;
+float a,b,div;
+cout<<"Enter first number"<<endl;
+cin>>a;
+cout<<"Enter second number"<<endl;
+cin>>b;
+sum = a + b;
+sub = a - b;
+prod = a * b;
+div = a / b;
+cout<<"The sum is:"<<endl;
+cout<<sum<<endl;
+cout<<"The subtraction is:"<<endl;
+cout<<sub<<endl;
+cout<<"The product is:"<<endl;
+cout<<prod<<endl;
+cout<<"The division is:"<<endl;
+cout<<div<<endl;
+}
+/*output
+Enter first number
+7
+Enter second number
+9
+The sum is:
+16
+The subtraction is:
+-2
+The product is:
+63
+The division is:
+0.777778*/
